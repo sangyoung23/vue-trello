9b03613ea29846b913f52fb72e30c18d9fa400e0
sangyoung23 <rmarkd2212@naver.com>
2023-04-27 18:30:40 +0900

    상세페이지 작업상태 진행중

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -1,18 +1,42 @@
 <template>
-  <div>
-    Detail
+  <div class="detail">
+    <h1 >{{ attr }}</h1>
     {{ todo.option1 }}
     {{ todo.option2 }}
+    <select @change="changeAttr($event)" class="form-select" aria-label="Default select example" v-model="attr">
+      <option>doing</option>
+      <option>done</option>
+    </select>
   </div>
 </template>
 
 <script setup>
+import { ref, watch, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const todo = JSON.parse(route.params.todo)
+const attr = ref('');
 
-console.log(route.params)
+const done = ref('')
+const doing = ref('')
+
+const changeAttr = (event) => {
+    if (event.target.value === 'done') {
+        todo.attr = true
+        console.log('완료',todo.attr)
+    } else if (event.target.value === 'doing') {
+        todo.attr = false
+        console.log('진행중',todo.attr)
+    }
+}
+
+const init = () => {
+    todo.attr ? done.value = 'done' : doing.value = 'doing'
+}
+init()
+
+
 </script>
 
 <style scoped></style>
--- src/stores/list.js
+++ src/stores/list.js
@@ -7,14 +7,14 @@ export const useTodoStore = defineStore({
   }),
   actions: {
     // toods 배열에 추가 핸들러
-    addTodo (todo) {
-        this.todos.push({ ...todo, done: false })
+    addTodo(todo) {
+      this.todos.push({ ...todo, attr: false })
+      console.log(this.todos)
     },
     // todos 배열안 값 삭제 핸들러
-    removeTodo (todo) {
-               const index = this.todos.indexOf(todo)
-        this.todos.splice(index, 1)
+    removeTodo(todo) {
+      const index = this.todos.indexOf(todo)
+      this.todos.splice(index, 1)
     }
   }
 })
-
